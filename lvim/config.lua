c63bfe495dabd8de95b907d49504bb5b3a24f627
Raymond Boswel <raymondboswel@gmail.com>
2023-11-02 15:41:02 +0200

    Updated lvim config via cron

--- lvim/config.lua
+++ lvim/config.lua
@@ -21,8 +21,8 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
--- lvim.keys.normal_mode["<C-J>"] = ":cnext<CR>"
--- lvim.keys.normal_mode["<C-K>"] = ":cprev<CR>"
+lvim.keys.normal_mode["<C-q-j>"] = ":cnext<CR>"
+lvim.keys.normal_mode["<C-q-k>"] = ":cprev<CR>"
 
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
