cd28697be688afafcf1180e2f7257c3f24ee97db
Raymond Boswel <raymondboswel@gmail.com>
2023-11-30 10:21:01 +0200

    Updated lvim config via cron

--- lvim/config.lua
+++ lvim/config.lua
@@ -63,7 +63,6 @@ lvim.keys.normal_mode["<C-q-k>"] = ":cprev<CR>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
