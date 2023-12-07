58500263ebb1d9c22994fb2405e95a2f01c0b66a
Raymond Boswel <raymondboswel@gmail.com>
2023-12-07 08:56:01 +0200

    Updated lvim config via cron

--- lvim/config.lua
+++ lvim/config.lua
@@ -24,6 +24,7 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<C-q-j>"] = ":cnext<CR>"
 lvim.keys.normal_mode["<C-q-k>"] = ":cprev<CR>"
 lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.builtin.terminal.open_mapping = "<c-t>"
 
 
 -- unmap a default keymapping
