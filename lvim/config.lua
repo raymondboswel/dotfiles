49fce2d136abaa12bec7b3a729171ed7d8719e2d
Raymond Boswel <raymondboswel@gmail.com>
2024-03-28 11:38:00 +0200

    Updated lvim config via cron

--- lvim/config.lua
+++ lvim/config.lua
@@ -255,7 +255,7 @@ lvim.plugins = {
 }
 
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 -- vim.cmd('colorscheme flexoki')
 -- lvim.colorscheme = "flexoki"
